8877d6c19abd4f5a594b1d5b6cd5239320b22742
tomas berhane <tomib@tomass-MacBook-Air.local>
2021-05-21 15:25:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello")
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+*/
+
+/* EXERCISE 1
+Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
+*/
+
+function areaOfRectangle(l1,l2){
+    return l1 * l2
+}
